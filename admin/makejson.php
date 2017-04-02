7cb539ae7a48c20971ab7d96e19440aceb341533
kilianso <info@kilian.so>
2017-04-02 14:58:49 +0200

    make sure json file gets written and updated

--- admin/makejson.php
+++ admin/makejson.php
@@ -1,15 +1,6 @@
 <?php
-   $json = $_POST['json'];
-
-   /* sanity check */
-   if (json_decode($json) != null)
-   {
-     $file = fopen('data.json','w+');
-     fwrite($file, $json);
-     fclose($file);
-   }
-   else
-   {
-	 	echo "Object Not Received";
-   }
+  $request_body = file_get_contents("php://input");
+  $myfile = fopen("data.json", "w") or die("Unable to open     file!");
+  fwrite($myfile, $request_body);
+  fclose($myfile);
 ?>
