a1f28964565615eaff6500cb98a2ce8a6f403677
kilianso <info@kilian.so>
2017-04-06 13:42:30 +0200

    save data to localstorage

--- game/scripts.js
+++ game/scripts.js
@@ -1,10 +1,23 @@
+var newData;
+var lsData;
+
+var timeNow;
+
 document.addEventListener("DOMContentLoaded", function() {
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', '../admin/data.json');
 	xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
 	xhr.onload = function() {
 		if (xhr.status === 200) {
-			console.log(xhr.responseText);
+			newData = JSON.parse(xhr.responseText);
+			lsData = JSON.parse(localStorage.getItem('data'));
+			if(newData.date != lsData.date){
+				console.log('update localStorage');
+				// update the localStorage if the date changed in the json file
+				localStorage.clear();
+				localStorage.setItem('data', xhr.responseText);
+				localStorage.setItem('hoursCounter', 10);
+			}
 		}else{
 			console.log("can't load data", xhr.status);
 		}
